d57376ba0010bd1930632f5ff81ee5d3d683051e
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-01-15 10:13:19 +0700

    nvim: adds mapping for telescope

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -3,8 +3,12 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local builtin = require('telescope.builtin')
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 
+map("n", "<leader>fs", builtin.lsp_document_symbols, {desc = "Telescope Symbol Picker"})
+map("n", "<leader>ds", builtin.diagnostics, {desc = "Telescope Diagnostics Picker"})
+map("n", "<leader>ts", builtin.treesitter, {desc = "Telescope Treesitter Picker"})
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

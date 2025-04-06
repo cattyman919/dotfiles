bd75fa0f21a5a1a55dbf893c2aa685b3116725f8
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-04-07 00:42:41 +0700

    fix lsp bug with mason-lsp

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,5 +1,5 @@
 -- load defaults i.e lua_lsp
--- require("nvchad.configs.lspconfig").defaults()
+require("nvchad.configs.lspconfig").defaults()
 
 require("mason").setup()
 require("mason-lspconfig").setup({
@@ -22,40 +22,13 @@ require("mason-lspconfig").setup({
 local lspconfig = require("lspconfig")
 local nvlsp = require("nvchad.configs.lspconfig")
 
--- EXAMPLE
--- local servers = {
--- 	"html",
--- 	"cssls",
--- 	"lua_ls",
--- 	"clangd",
--- 	"gopls",
--- 	"ts_ls",
--- 	"yamlls",
--- 	"html",
--- 	"mdx_analyzer",
--- 	"rust_analyzer",
--- 	"texlab",
--- 	"basedpyright",
--- }
-
--- lsps with default config
--- for _, lsp in ipairs(servers) do
--- 	lspconfig[lsp].setup({
--- 		on_attach = nvlsp.on_attach,
--- 		on_init = nvlsp.on_init,
--- 		capabilities = nvlsp.capabilities,
--- 	})
--- end
-
 -- setup with mason-lspconfig
 require("mason-lspconfig").setup_handlers({
-	function(server)
-		lspconfig[server].setup({
-			{
+	function(server_name)
+		lspconfig[server_name].setup({
 				on_attach = nvlsp.on_attach,
 				on_init = nvlsp.on_init,
 				capabilities = nvlsp.capabilities,
-			},
 		})
 	end,
 })

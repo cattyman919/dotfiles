f95e0b4ef735ebe62863ac21283775f375d8cd92
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-06-02 17:55:55 +0700

    added custom rofi scripts

--- polybar/scripts/power.sh
+++ polybar/scripts/power.sh
@@ -1,11 +1,16 @@
 #! /bin/sh
 
-chosen=$(printf "Power Off\nRestart\nLock" | rofi -dmenu -i)
+options="Lock\0icon\x1fsystem-lock-screen\n"
+options="${options}Suspend\0icon\x1fsystem-suspend\n" 
+options="${options}Restart\0icon\x1fsystem-reboot\n"
+options="${options}Power Off\0icon\x1fsystem-shutdown"
+
+chosen=$(printf "$options" | rofi -sep '\n' -window-title "Power Menu" -dmenu -markup-rows -config ~/.config/rofi/power.rasi)
 
 case "$chosen" in
-  "Power Off") poweroff ;;
-  "Restart") reboot ;;
   "Lock") slock ;;
+  "Restart") reboot ;;
+  "Suspend") systemctl suspend-then-hibernate ;;
+  "Power Off") poweroff ;;
   *) exit 1 ;;
 esac
-

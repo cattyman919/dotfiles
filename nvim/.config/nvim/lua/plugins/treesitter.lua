607831a1da79cd02fa0cf39663cb94e440d5ac0d
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-06-03 23:05:42 +0700

    added sooooo many nvim plugins

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,59 @@
 return {
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-	require'nvim-treesitter.configs'.setup {
-	    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+      require("nvim-treesitter.configs").setup {
+        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-	    -- Automatically install missing parsers when entering buffer
-	    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	    auto_install = false,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = false,
 
-	    highlight = {
-		enable = true,
+        highlight = {
+          enable = true,
 
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
-		
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		disable = function(lang, buf)
-		    local max_filesize = 100 * 1024 -- 100 KB
-		    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-		    if ok and stats and stats.size > max_filesize then
-			return true
-		    end
-		end,
-		additional_vim_regex_highlighting = false,
-	    },
-	}
-    end
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
+          -- disable = { "c", "rust" },
+
+          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  },
+  -- NOTE: js,ts,jsx,tsx Auto Close Tags
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
+    config = function()
+      -- Independent nvim-ts-autotag setup
+      require("nvim-ts-autotag").setup {
+        opts = {
+          enable_close = true, -- Auto-close tags
+          enable_rename = true, -- Auto-rename pairs
+          enable_close_on_slash = false, -- Disable auto-close on trailing `</`
+        },
+        per_filetype = {
+          ["html"] = {
+            enable_close = true, -- Disable auto-closing for HTML
+          },
+          ["typescriptreact"] = {
+            enable_close = true, -- Explicitly enable auto-closing (optional, defaults to `true`)
+          },
+        },
+      }
+    end,
+  },
 }
-

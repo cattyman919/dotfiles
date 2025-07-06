7304f8498854a5e22bb5ea528d277ff818f2899d
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-07-07 01:22:53 +0700

    feat: breadcrumb neovim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -22,30 +22,5 @@ return {
 				},
 			},
 		})
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<space>fh", builtin.help_tags, { desc = "Find help tags" })
-
-		vim.keymap.set("n", "<space>ff", function()
-			-- Check if is in a git repo
-			builtin.find_files({ desc = "Find files" })
-			-- vim.fn.system("git rev-parse --is-inside-work-tree > /dev/null 2>&1")
-			--
-			-- if vim.v.shell_error == 0 then
-			-- 	builtin.git_files({ desc = "Find git files" })
-			-- else
-			-- 	builtin.find_files({ desc = "Find files" })
-			-- end
-		end, { desc = "Find files (Git or All)" })
-
-		vim.keymap.set("n", "<space>fo", builtin.oldfiles, { desc = "Find old files" })
-		vim.keymap.set("n", "<space>fj", builtin.jumplist, { desc = "Find jump list" })
-		vim.keymap.set("n", "<space>fw", builtin.live_grep, { desc = "Live Grep" })
-
-		-- Telescope to Neovim Config Files
-		vim.keymap.set("n", "<space>en", function()
-			require("telescope.builtin").find_files({
-				cwd = vim.fn.stdpath("config"),
-			})
-		end)
 	end,
 }

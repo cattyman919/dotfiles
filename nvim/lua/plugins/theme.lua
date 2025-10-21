8d2944a63b41db25659ee29f9827fa547ce38a94
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-10-22 00:44:54 +0700

    chores

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -12,7 +12,7 @@ return {
 
 			vim.api.nvim_set_hl(0, "IblIndent", { fg = "#414868" })
 			vim.api.nvim_set_hl(0, "IblScope", { fg = "#7aa2f7" })
-			vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 			vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none", fg = "#7aa2f7", bold = true })
 			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 		end,

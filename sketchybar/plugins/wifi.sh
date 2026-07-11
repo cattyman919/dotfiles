b082d890b29140fc6f321e51ea0549146771101f
Seno Pamungkas <senop.rahman@gmail.com>
2026-07-11 22:29:25 +0700

    feat (sketchybar): wifi

---
+++ sketchybar/plugins/wifi.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -u
+
+CONFIG_DIR="/Users/senohebat/.config/sketchybar"
+THEME_DIR="$CONFIG_DIR/theme"
+
+source "$THEME_DIR/colors.sh"
+
+ICON=""
+
+# Find the Wi-Fi hardware port dynamically
+WIFI_DEVICE=$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $2}')
+
+if [ -z "$WIFI_DEVICE" ]; then
+  sketchybar --set "$NAME" drawing=off
+  exit 0
+fi
+
+SSID=$(ipconfig getsummary "$WIFI_DEVICE" 2>/dev/null | awk -F': ' '/ SSID / {print $2}')
+
+if [ -n "$SSID" ]; then
+  if [ ${#SSID} -gt 20 ]; then
+    SSID="$(echo "$SSID" | cut -c1-17)..."
+  fi
+
+  sketchybar --set "$NAME" \
+    drawing=on \
+    icon="$ICON" \
+    icon.color="${DEFAULT}" \
+    label="$SSID" \
+    label.color="${DEFAULT}"
+else
+  sketchybar --set "$NAME" \
+    drawing=on \
+    icon="$ICON" \
+    icon.color="${RED}" \
+    label="Off" \
+    label.color="${RED}"
+fi
